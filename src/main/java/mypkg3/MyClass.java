6817dbe7e8b2d2dae9cc259f3047e0b83f3aec66
raju0698 <kraju0698@gmail.com>
2024-09-12 13:02:39 +0530

    latest commit

---
+++ src/main/java/mypkg3/MyClass.java
@@ -0,0 +1,18 @@
+package mypkg3;
+
+public class MyClass {
+	
+	public int sum(int a, int b)
+	{
+		int c;
+		int d;
+		c=10+15;
+		return c;
+	}
+
+	public static void main(String[] args) {
+		MyClass ob = new MyClass();
+		System.out.println(ob.sum(20,30));
+	}
+
+}
